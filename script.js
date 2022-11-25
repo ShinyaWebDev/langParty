8dd317dcb6655925fe488465004ef60f112b0fe9
ShinFunk <shinya.yoshida420@gmail.com>
2022-11-25 15:24:23 +1100

    yay

--- script.js
+++ script.js
@@ -111,3 +111,38 @@ window.addEventListener('load', function() {
 })
 
 
+const output = document.querySelector("h3");
+
+const countDown = () => {
+  const input = document.querySelector("input");
+  const countDownDate = new Date(input.value).getTime();
+  
+  const now = new Date().getTime();
+  
+  const distance = countDownDate - now;
+  
+  const dd = Math.floor(
+    distance / (1000 * 60 * 60 * 24)
+  );
+
+  const hh = Math.floor(
+    (distance % (1000 * 60 * 60 * 24))/(1000 * 60 * 60)
+  );
+  
+  const mm = Math.floor(
+    (distance % (1000 * 60 * 60)) / (1000 * 60)
+  );
+  
+  const ss = Math.floor(
+    (distance % (1000 * 60)) / 1000
+  );
+  
+  output.innerText = dd + 'd ' + hh + 'h ' + mm + 'm ' + ss +'s';
+
+  if(distance < 0){
+    output.innerText = "Time has come!"
+  }
+}
+
+countDown();
+setInterval(countDown,1000);
